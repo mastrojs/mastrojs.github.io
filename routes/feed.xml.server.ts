00008638bae9e1da22a41b42b39f92b9b23c284a
mb21 <mb21@users.noreply.github.com>
2025-09-14 12:00:20 +0200

    Use mastro/feed

--- routes/feed.xml.server.ts
+++ routes/feed.xml.server.ts
@@ -1,24 +1,17 @@
-import { html, htmlToResponse } from "mastro";
+import { atomResponse } from "mastro/feed";
 
 const baseUrl = "https://mastrojs.github.io/";
+const updated = new Date("2025-09-10");
 
-export const GET = async () => {
-  const feed = html`<?xml version="1.0" encoding="utf-8"?>
-    <feed xmlns="http://www.w3.org/2005/Atom">
-      <title>Mastro</title>
-      <subtitle>
-        Updates about the web framework and static site generator, as well as the Mastro Guide.
-      </subtitle>
-      <link rel="self" href="${baseUrl}feed.xml" />
-      <updated>2025-09-10T18:00:00Z</updated>
-      <author>
-        <name>Mauro Bieg</name>
-      </author>
-      <id>${baseUrl}</id>
-    </feed>
-  `;
-
-  const res = await htmlToResponse(feed);
-  res.headers.set("Content-Type", "application/atom+xml");
-  return res;
-};
+export const GET = () =>
+  atomResponse({
+    title: "Mastro",
+    subtitle: "Updates about the web framework and static site generator, as well as the Mastro Guide.",
+    id: new URL(baseUrl),
+    linkSelf: new URL("feed.xml", baseUrl),
+    updated,
+    author: {
+      name: "Mauro Bieg"
+    },
+    entries: [],
+  });
